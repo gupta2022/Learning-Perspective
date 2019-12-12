cf2d1da42e356c1d48ae001eead0d4689dc832eb
Gaurav Gupta <guptagaurav2022@gmail.com>
2019-12-12 14:12:01 +0000

    Testing

--- blob-test.py
+++ blob-test.py
@@ -1,1 +1,15 @@
 
+import nltk
+    # nltk.download('punkt')
+    # nltk.download('averaged_perceptron_tagger')
+    # nltk.download('brown')
+    # pip install textblob
+import textblob
+text1=open('sample.txt')
+text=text1.read()
+#print(text)
+blob=textblob.TextBlob(text)
+
+#print(blob)
+#print(blob.tags)
+print(blob.noun_phrases)
